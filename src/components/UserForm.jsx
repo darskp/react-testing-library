fef562c0c6d57b75d55ee90521334922e0557bae
darskp <darshanpatgar100@gmail.com>
2023-12-06 14:35:19 +0530

    userform setup

---
+++ src/components/UserForm.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const UserForm = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("NAME", name);
+        console.log("Email", email);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>
+                    Name : &nbsp;
+                </label>
+                <input value={name} onChange={(e) => setName(e.target.value)} />
+            </div>
+            <div>
+                <label>
+                    Email : &nbsp;
+                </label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <button>Add User</button>
+        </form>
+    );
+}
+
+export default UserForm;
