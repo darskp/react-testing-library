abba4dad032f9f1a01cfc9ab8e81a89701a39aff
darskp <darshanpatgar100@gmail.com>
2023-12-07 15:56:23 +0530

    rtl-book serve

--- testing-library-notes.js
+++ testing-library-notes.js
@@ -8,7 +8,7 @@
 // jest - runs our tests and reports results
 // jsdom - when you npm run test, it creates a fake browser environment in a node environment
 
-import { screen, render } from '@testing-library/react'; // to render the reactdom
+import { screen, render, waitFor } from '@testing-library/react'; // to render the reactdom
 import userEvent from '@testing-library/user-event'; // helps simulate user actions like event,click,type
 import UserForm from './UserForm';
 import { element } from 'prop-types';
@@ -24,3 +24,12 @@ import { element } from 'prop-types';
 
 // screen.getByRole()
 // screen.findByTitle()
+
+
+// await waitFor(()=>{
+// }) -- you give component some time to update its state before making assertion
+
+
+//npx rtl-book serve roles-notes.js
+
+
